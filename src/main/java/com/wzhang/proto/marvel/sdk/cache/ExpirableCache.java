8f0420fdb5b400bf654313d1e7292f98c667a598
willi0naire <70806950+willi0naire@users.noreply.github.com>
2025-02-08 23:02:28 +0800

    Update ExpirableCache.java

--- src/main/java/com/wzhang/proto/marvel/sdk/cache/ExpirableCache.java
+++ src/main/java/com/wzhang/proto/marvel/sdk/cache/ExpirableCache.java
@@ -1,5 +1,6 @@
 package com.wzhang.proto.marvel.sdk.cache;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
@@ -14,7 +15,7 @@ public class ExpirableCache<K, V> implements MyCache<K, V> {
 	private final String HIT = "HIT";
 	private final String MISS = "MISS";
 	private final ConcurrentMap<K, V> cache = new ConcurrentHashMap<>();
-	private final ConcurrentMap<K, Long> timestamps = new ConcurrentHashMap<>();
+	private final ConcurrentMap<K, Long> expirationTimes = new ConcurrentHashMap<>();
 	private final ConcurrentMap<String, Long> hitMissMap = new ConcurrentHashMap<>();
 	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	private final long expirationDuration;
@@ -42,22 +43,22 @@ public class ExpirableCache<K, V> implements MyCache<K, V> {
 
 	@Override
 	public V put(K key, V value) {
-		timestamps.put(key, System.nanoTime());
+		expirationTimes.put(key, System.nanoTime() + timeUnit.toNanos(expirationDuration));
 		return cache.put(key, value);
 	}
 
 	@Override
 	public V putIfAbsent(K key, V value) {
-		timestamps.putIfAbsent(key, System.nanoTime());
+		expirationTimes.putIfAbsent(key, System.nanoTime() + timeUnit.toNanos(expirationDuration));
 		return cache.putIfAbsent(key, value);
 	}
 
 	@Override
 	public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-		timestamps.putIfAbsent(key, System.nanoTime());
+		expirationTimes.putIfAbsent(key, System.nanoTime() + timeUnit.toNanos(expirationDuration));
 		return cache.computeIfAbsent(key, k -> {
 			V value = mappingFunction.apply(k);
-			timestamps.put(k, System.nanoTime());
+			expirationTimes.put(k, System.nanoTime());
 			return value;
 		});
 	}
@@ -65,7 +66,7 @@ public class ExpirableCache<K, V> implements MyCache<K, V> {
 	@Override
 	public boolean remove(K key) {
 		final var V = cache.remove(key);
-		timestamps.remove(key);
+		expirationTimes.remove(key);
 		hitMissMap.put(HIT, 0L);
 		hitMissMap.put(MISS, 0L);
 		if (null != V)
@@ -75,10 +76,10 @@ public class ExpirableCache<K, V> implements MyCache<K, V> {
 
 	private void removeExpiredEntries() {
 		System.out.println("evicting cache...");
-		long expirationThreshold = System.nanoTime() - timeUnit.toNanos(expirationDuration);
-		for (K key : timestamps.keySet()) {
-			if (timestamps.get(key) < expirationThreshold) {
-				System.out.println(String.format("removing expired cache with key=%s result=%s", key, remove(key)));
+		for (K key : expirationTimes.keySet()) {
+			if (null == expirationTimes.get(key) || expirationTimes.get(key) <= System.nanoTime()) {
+				System.out.println(String.format("%s removing expired cache with key=%s result=%s", LocalDateTime.now(),
+						key, remove(key)));
 			}
 		}
 		System.out.println("done evicting cache...");
